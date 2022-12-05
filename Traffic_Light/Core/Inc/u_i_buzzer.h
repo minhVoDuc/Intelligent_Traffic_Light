cf59400a3322a74f9035ab653b42a60568fad47a
Vo Duc Minh <92356136+minhVoDuc@users.noreply.github.com>
2022-12-06 00:15:12 +0700

    add buzzer module

--- Traffic_Light/Core/Inc/u_i_buzzer.h
+++ Traffic_Light/Core/Inc/u_i_buzzer.h
@@ -8,6 +8,13 @@
 #ifndef INC_U_I_BUZZER_H_
 #define INC_U_I_BUZZER_H_
 
+#include "main.h"
 
+extern TIM_HandleTypeDef htim3;
+
+void buzzer_modify_volume(uint8_t volumeLevel); //modify volume of buzzer at volumeLevel
+void buzzer_turn_on(); //turn on buzzer
+void buzzer_turn_off(); //turn off buzzer or volume = 0
+void buzzer_blinky(); //buzzer turn on and off periodly
 
 #endif /* INC_U_I_BUZZER_H_ */
--- Traffic_Light/Core/Src/u_i_buzzer.c
+++ Traffic_Light/Core/Src/u_i_buzzer.c
@@ -6,3 +6,22 @@
  */
 
 #include "u_i_buzzer.h"
+
+TIM_HandleTypeDef htim3;
+
+void buzzer_modify_volume(uint8_t volumeLevel) {
+  __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, volumeLevel);
+}
+
+void buzzer_turn_on() {
+	buzzer_modify_volume(100);
+}
+
+void buzzer_turn_off() {
+	buzzer_modify_volume(0);
+}
+
+void buzzer_bliny() {
+	if (buzzer_state == BZ_ON) buzzer_turn_on();
+	else buzzer_turn_off();
+}
