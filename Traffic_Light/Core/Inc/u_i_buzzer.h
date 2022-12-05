ff796926e1f3a01840868ab90da6be371ef3b4a1
LPT0317 <75077760+LPT0317@users.noreply.github.com>
2022-12-05 15:20:30 +0700

    buzzer

--- Traffic_Light/Core/Inc/u_i_buzzer.h
+++ Traffic_Light/Core/Inc/u_i_buzzer.h
@@ -8,6 +8,10 @@
 #ifndef INC_U_I_BUZZER_H_
 #define INC_U_I_BUZZER_H_
 
+#include "main.h"
 
+void buzzer_modify_volume(uint8_t volumeLevel); //chỉnh volume buzzer ở mức volumeLevel
+void buzzer_turn_on(); //mo buzzer
+void buzzer_turn_off(); //tắt buzzer hay volume = 0
 
 #endif /* INC_U_I_BUZZER_H_ */
--- Traffic_Light/Core/Src/u_i_buzzer.c
+++ Traffic_Light/Core/Src/u_i_buzzer.c
@@ -6,3 +6,20 @@
  */
 
 #include "u_i_buzzer.h"
+
+uint8_t volume = 0;
+
+void buzzer_modify_volume(uint8_t volumeLevel)
+{
+  volume = volumeLevel;
+}
+
+void buzzer_turn_on()
+{
+  __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, volume);
+}
+
+void buzzer_turn_off()
+{
+  __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1, 0);
+}
